ec37d56cd603ec22944be11208a3b225c32fdeab
yyon <linkkirby7@gmail.com>
2016-01-23 12:49:12 -0500

    Replaced spaces by tabs

--- data/main.lua
+++ data/main.lua
@@ -5,20 +5,20 @@
 local game_manager = require("scripts/game_manager")
 
 function sol.main:on_started()
-  -- This function is called when Solarus starts.
-  print("This is a sample quest for Solarus.")
+	-- This function is called when Solarus starts.
+	print("This is a sample quest for Solarus.")
 
-  -- Setting a language is useful to display text and dialogs.
-  sol.language.set_language("en")
+	-- Setting a language is useful to display text and dialogs.
+	sol.language.set_language("en")
 
-  -- Show the Solarus logo initially.
-  local solarus_logo = require("menus/solarus_logo")
-  sol.menu.start(self, solarus_logo)
+	-- Show the Solarus logo initially.
+--	local solarus_logo = require("menus/solarus_logo")
+--	sol.menu.start(self, solarus_logo)
 
-  -- Start the game when the Solarus logo menu is finished.
-  solarus_logo.on_finished = function()
-    game_manager:start_game()
-  end
+	-- Start the game when the Solarus logo menu is finished.
+--	solarus_logo.on_finished = function()
+		game_manager:start_game()
+--	end
 
 end
 
