07a4dd67bce6d0e9a6320696d930a7192a671034
Josh Newlin <jnewlin@purdue.edu>
2016-02-28 21:21:49 -0500

    new and improved way to render sword health and souls

--- data/scripts/hud/possess_o_meter.lua
+++ data/scripts/hud/possess_o_meter.lua
@@ -20,7 +20,7 @@ function possess_o_meter:initialize(game)
   	self.surface = sol.surface.create(1904, 100)
   	self.dst_x = 0
   	self.dst_y = 0
-	self.stage_displayed = 0
+	self.stage = 0
   	self.all_sword_img = sol.surface.create("hud/sword_soul_meter.png")
 	self.sword_anim_setup = sol.sprite.create("hud/soul_meter")
 end
@@ -34,53 +34,31 @@ end
 
 --  check heart data and fix periodically
 function possess_o_meter:check()
-
-  	local need_rebuild = false
 	
 	local stage = 16
 	
 	hero = game:get_hero()
     	if hero.entitydata ~= nil then
-		
-		stage = math.floor(hero.souls * 16)
-		--print(math.floor(hero.souls * 16))
+		self.stage = hero.souls
 	end
 
-	if stage > 16 then
-		stage = 16
-	end
-	if stage < 0 then
-		stage = 0
-	end
-
-	if stage ~= self.stage_displayed then
-		need_rebuild = true
-		self.stage_displayed = stage
-		
-	end
 
 	if self.game:is_started() then
 
-    		if self.stage_displayed <= 4
+    		if self.stage <= 4
         			and not self.game:is_suspended() then
-      			need_rebuild = true
       			if self.sword_anim_setup:get_animation() ~= "danger" then
         				self.sword_anim_setup:set_animation("danger")
       			end
     		elseif self.sword_anim_setup:get_animation() ~= "normal" then
-      			need_rebuild = true
       			self.sword_anim_setup:set_animation("normal")
     		end
   	end
 
 	
 
-  	-- redraw only if something has changed
-  	if need_rebuild then
-    		self:rebuild_surface()
-  	end
-
-	--self.sword_anim_setup:set_animation("sword progress bar")
+  	
+    	self:rebuild_surface()
 
   	-- check again in 50ms
   	sol.timer.start(self, 50, function()
@@ -93,7 +71,8 @@ function possess_o_meter:rebuild_surface()
 
   	self.surface:clear()
 	self.sword_anim_setup:draw(self.surface,0,0)
-	self.all_sword_img:draw_region(self.stage_displayed * 112, 0, 112, 50, self.surface, 0, 0)
+	self.all_sword_img:draw_region(0,0,112,50,self.surface,0,0)
+	self.all_sword_img:draw_region(1792, 0, math.floor(self.stage*112), 50, self.surface, 0, 0)
 	
 end
 
