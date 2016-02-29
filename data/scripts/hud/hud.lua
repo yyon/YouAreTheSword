23ba6c0270734190952fa920656b9aa2a9a92d88
Josh Newlin <jnewlin@purdue.edu>
2016-02-28 21:06:12 -0500

    fixed resolution and visibility of the swords in hud, but warning animation is broken now

--- data/scripts/hud/hud.lua
+++ data/scripts/hud/hud.lua
@@ -13,12 +13,17 @@ function hud_manager:create(game)
 
 	local health_builder = require("scripts/hud/health")
 	local soul_builder = require("scripts/hud/possess_o_meter")
+	local sword_health_builder = require("scripts/hud/sword_health")
 
 	local menu = health_builder:new(game)
   	menu:set_dst_position(0, 10)
   	hud.elements[#hud.elements + 1] = menu
 
 	local menu = soul_builder:new(game)
+  	menu:set_dst_position(10, 65)
+  	hud.elements[#hud.elements + 1] = menu
+
+	local menu = sword_health_builder:new(game)
   	menu:set_dst_position(10, 10)
   	hud.elements[#hud.elements + 1] = menu
 
