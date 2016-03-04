1f521bb6d632ed1ce7630f850fc1ffcc14122f2c
rhayex <rhayex.2@gmail.com>
2016-03-04 01:30:59 -0500

    Changed some stuff with teleport

---
+++ data/abilities/teleport.lua
@@ -0,0 +1,35 @@
+local class = require "middleclass"
+Ability = require "abilities/ability"
+require "scripts/movementaccuracy"
+
+
+function Teleport:initialize(entitydata)
+	Ability.initialize(self, entitydata, "NormalAbility", 300, 0, 50, true)
+end
+
+
+
+
+
+
+function TeleportAbility:doability()
+	tox, toy = self.entitydata:gettargetpos()
+	tox, toy = self:withinrange(tox, toy)
+	
+--	self.entitydata:setanimation("walking")
+	
+--	local angle = self.entitydata.entity:get_angle(tox, toy)
+--	local dist = self.entitydata.entity:get_distance(tox, toy)
+	
+--	self.movement = sol.movement.create("straight")
+--	self.movement:set_speed(1000)
+--	self.movement:set_target(tox, toy)
+--	self.movement:set_angle(angle)
+--	self.movement:set_max_distance(dist)
+--	self.movement:start(self.entitydata.entity)
+
+	self.setposition(tox, toy)
+	
+	
+--	movementaccuracy(self.movement, angle, self.entitydata.entity)
+end
