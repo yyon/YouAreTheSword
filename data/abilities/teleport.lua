89c0f3d494f4137fd5d32043ec48b1245462b066
yyon <linkkirby7@gmail.com>
2016-03-04 19:04:11 -0500

    Cleaned up teleport code

--- data/abilities/teleport.lua
+++ data/abilities/teleport.lua
@@ -8,38 +8,13 @@ function TeleportAbility:initialize(entitydata)
 	Ability.initialize(self, entitydata, "Teleporter", 600, 0, 2550, true)
 end
 
-
-
 function TeleportAbility:doability()
 	tox, toy = self.entitydata:gettargetpos()
 	tox, toy = self:withinrange(tox, toy)
 	
---	self.entitydata:setanimation("walking")
-	
---	local angle = self.entitydata.entity:get_angle(tox, toy)
---	local dist = self.entitydata.entity:get_distance(tox, toy)
-	
---	self.movement = sol.movement.create("straight")
---	self.movement:set_speed(1000)
---	self.movement:set_target(tox, toy)
---	self.movement:set_angle(angle)
---	self.movement:set_max_distance(dist)
---	self.movement:start(self.entitydata.entity)
-
 	self.entitydata.entity:set_position(tox, toy)
 
 	self:finish()
-	
---	movementaccuracy(self.movement, angle, self.entitydata.entity)
-end
-
-
-function TeleportAbility:oncancel()
-	--self.movement:stop()
-	self.finish()
-end
-
-function TeleportAbility:onfinish()
 end
 
 return TeleportAbility
