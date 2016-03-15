f0f667bf0703f3655866c5e7a0a1de31d6870dcc
yyon <linkkirby7@gmail.com>
2016-03-14 21:11:27 -0400

    Fixed bug with possession

--- data/abilities/possess.lua
+++ data/abilities/possess.lua
@@ -6,14 +6,16 @@ PossessAbility = Ability:subclass("PossessAbility")
 local Effects = require "enemies/effect"
 
 function PossessAbility:initialize(entitydata)
-	Ability.initialize(self, entitydata, "Possess", 20000, "possess", 0, 10000, true, "casting")
+	Ability.initialize(self, entitydata, "Possess", 20000, "possess", 0, 500, true, "casting")
 end
 
 function PossessAbility:doability()
 	tox, toy = self.entitydata:gettargetpos()
 	self.target = self.entitydata:getclosestentity(tox, toy, true)
 	
-	Effects.PossessEffect:new(self.target, self.entitydata.team, 20000)
+	if not self.target.entity.ishero then
+		Effects.PossessEffect:new(self.target, self.entitydata.team, 20000)
+	end
 	
 	self:finish()
 end
