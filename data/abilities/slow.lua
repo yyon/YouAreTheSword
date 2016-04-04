1c6c6b026888c9068afc245338b431effe20587c
rhayex <rhayex.2@gmail.com>
2016-04-03 23:09:15 -0400

    Added a "slow" ability. I have no idea if I'm doing this right.

---
+++ data/abilities/slow.lua
@@ -0,0 +1,32 @@
+local class = require "middleclass"
+local Ability = require "abilities/ability"
+
+local Effects = require "enemies/effect"
+
+local SlowAbility = Ability:subclass("Slow")
+
+local Effects = require "enemies/effect"
+
+function SlowAbility:doability()
+	local tox, toy = self:gettargetpos()
+	tox, toy = self:withinrange(tox, toy)
+
+	local entity = self.entitydata.entity
+	local map = entity:get_map()
+	local x,y,layer = entity:get_position()
+	local w,h = entity:get_size()
+	local entitydata = self.entitydata
+
+--	self.slowentity = map:create_custom_entity({model="slow", x=tox, y=toy, layer=2, direction=0, width=w, height=h})
+--	self.slowentity.ability = self
+--	self.slowentity:start(tox, toy)
+
+	self:AOE(200, 4, {electric=3000, dontblock=true}, self.lightningentity)
+
+	sol.audio.play_sound("thunder")
+
+	self:finish()
+end
+
+
+return SlowAbility
