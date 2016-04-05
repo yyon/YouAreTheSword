cf1afaa0c620b1782fa6aac3b0902e6a36acfe7f
yyon <linkkirby7@gmail.com>
2016-04-04 21:03:22 -0400

    CReated lightningball ability

---
+++ data/abilities/lightningball.lua
@@ -0,0 +1,30 @@
+local class = require "middleclass"
+local Ability = require "abilities/ability"
+
+local Effects = require "enemies/effect"
+
+local FireballAbility = Ability:subclass("FireballAbility")
+
+function FireballAbility:initialize(entitydata)
+	Ability.initialize(self, entitydata, "Lightning Ball", 800, "fireball", 500, 2000, true, "casting")
+end
+
+function FireballAbility:doability()
+	local tox, toy = self:gettargetpos()
+	self.tox, self.toy = tox, toy
+
+	local entity = self.entitydata.entity
+	local map = entity:get_map()
+	local x,y,layer = entity:get_position()
+	local w,h = entity:get_size()
+	local entitydata = self.entitydata
+
+	self.fireballentity = map:create_custom_entity({model="lightningball", x=x, y=y, layer=layer, direction=0, width=w, height=h})
+	self.fireballentity:start(self, tox, toy)
+
+--	sol.audio.play_sound("fireball")
+
+	self:finish()
+end
+
+return FireballAbility
---
+++ data/entities/lightningball.lua
@@ -0,0 +1,18 @@
+local entity = ...
+
+local Effects = require "enemies/effect"
+
+local math = require "math"
+
+sol.main.load_file("entities/projectile")(entity)
+
+function entity:getdamage()
+  local aspects = {}
+  aspects.electric=1000
+  local damage = 0.5
+  return damage, aspects
+end
+
+function entity:getspritename()
+  return "abilities/lightningball"
+end
