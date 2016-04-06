2574e82786004a6b241354bfb1023f48dc0a2c24
yyon <linkkirby7@gmail.com>
2016-04-06 19:54:45 -0400

    Changed death, bomb, explosion animations changed death sound effect

--- data/entities/death.lua
+++ data/entities/death.lua
@@ -3,7 +3,7 @@ local entity = ...
 function entity:on_created()
 	self:set_optimization_distance(0)
 
-	self.sprite = self:create_sprite("enemies/enemy_killed")
+	self.sprite = self:create_sprite("adventurers/death")
 	self.sprite:set_paused(false)
 
 	function self.sprite.on_animation_finished (teleportsprite, sprite, animation)
