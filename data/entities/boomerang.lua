52a927bf3647d751d191e947b7a4010794fbc302
YXcs307 <js612789@gmail.com>
2016-04-07 10:59:00 -0400

    now boomerang only runs in half circle

--- data/entities/boomerang.lua
+++ data/entities/boomerang.lua
@@ -7,22 +7,17 @@ function entity:on_created()
 	self:set_optimization_distance(0)
 end
 
-function entity:start(tox, toy, hx, hy)
-	self.bom_sprite = self:create_sprite("abilities/boomerang")
-	local dist = self:get_distance(tox, toy)
-	if dist > self.ability.range then
-		dist = self.ability.range
-	end
+function entity:start(tox, toy)
+	self:create_sprite("abilities/boomerang")
 
 	local x, y = self:get_position()
-	local angle = self:get_angle(tox, toy) - math.pi
 	local movement = sol.movement.create("circle")
-	movement:set_center(hx,hy)
+	movement:set_center(x,y)
 	movement:set_radius(500)
 	movement:set_radius_speed(1000)
 	movement:set_clockwise(false)
 	movement:set_loop_delay(0)
-	movement:set_duration(2000)
+	movement:set_duration(100)
 	movement:start(self)
 
 end
