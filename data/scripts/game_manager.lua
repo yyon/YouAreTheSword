01a681031c34bd3212cd45d8fd2a44ca4928c52e
yyon <linkkirby7@gmail.com>
2016-04-13 18:42:34 -0400

    Fixed merge conflict

--- data/scripts/game_manager.lua
+++ data/scripts/game_manager.lua
@@ -400,9 +400,6 @@ function sol.main:on_key_released(key, modifiers)
 		return
 	end
 
-<<<<<<< HEAD
-	
-=======
 	if (key == "left alt") then
 		if self.helpmenu ~= nil then
 			sol.menu.stop(self.helpmenu)
@@ -415,7 +412,6 @@ function sol.main:on_key_released(key, modifiers)
 		return
 	end
 
->>>>>>> 0492bfa75ef22af573e44e52dcef451fac057630
 	local hero = game:get_hero()
 	if hero.entitydata == nil then
 		return
