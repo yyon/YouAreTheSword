01e46d834e7c9e474b885a64c4e6d4028eb4c91b
Josh Newlin <jnewlin@purdue.edu>
2016-04-28 16:19:53 -0400

    Adding spaceship map

---
+++ data/maps/spaceship/spaceship_1.lua
@@ -0,0 +1,19 @@
+local map = ...
+
+map.dialogprefix = "tutorial."
+
+local Effects = require "enemies/effect"
+
+function map:on_opening_transition_finished()
+	self:startcutscene()
+	self:camera("cam_pos")
+	self:camera("player")
+	self:say("player", "1", function()
+	self:say("player", "2", function()
+	end)
+	end)
+end
+
+function map:doend()
+	self:finish()
+end
