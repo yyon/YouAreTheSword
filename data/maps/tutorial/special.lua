614caae6b4d9944f9d751c3f02af5991827311c7
yyon <linkkirby7@gmail.com>
2016-08-15 16:23:40 -0400

    Working on special attack tutorial

--- data/maps/tutorial/special.lua
+++ data/maps/tutorial/special.lua
@@ -1,7 +1,23 @@
 local map = ...
 map.dialogprefix = "tutorialspecial."
 
+local Effects = require "enemies/effect"
+local LightningAbility = require "abilities/lightning"
+local SwordAbility = require "abilities/sword"
+local TransformAbility = require "abilities/swordtransform"
+
 function map:on_opening_transition_finished()
 	mage.entitydata.specialability = LightningAbility:new(mage.entitydata)
 	mage.entitydata.swordability = SwordAbility:new(mage.entitydata)
+	mage.entitydata.transformability = TransformAbility:new(mage.entitydata, "fire")
+	function combatdummy.entitydata:isvisible() return false end
+	combatdummy.entitydata.life = 1
+	combatdummy.entitydata.maxlife = 1
+	function combatdummy.entitydata:receivedamage(fromentitydata, damage, aspects)
+		print("DUMMY RECEIVE DAMAGE", aspects)
+		if aspects.fire == nil then
+			return true
+		end
+		return false
+	end
 end
